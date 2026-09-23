a2a89a3a0d92e5311574a94c35d0ea264d301866
alessandrokraemer-arch <alessandro.kraemer@impacta.edu.br>
2026-09-23 12:34:25 -0300

    Update CRM_mini.sql

--- conf/no mysql/CRM_mini.sql
+++ conf/no mysql/CRM_mini.sql
@@ -1,6 +1,10 @@
 -- aqui um banco de dados que em aula exploro como data warehouse...
+create user 'saas'@'0.0.0.0' identified by 'saas';
 create database enterprise;
+grant all privileges on enterprise .* to 'saas'@'0.0.0.0';
+flush privileges;
 use enterprise;
 create table funcionario (cpfPK varchar(14) not null primary key, primeiroNome varchar(50) not null, sobrenome varchar(50) not null);
 create table departamento (idPK int auto_increment primary key, nome varchar(50) not null, telefone varchar(20) not null, ramal varchar(4) not null);
 create table equipeatendimento (cpfPK varchar(14) not null primary key, dptoIdFK int not null, foreign key (cpfPK) references funcionario(cpfPK));
+exit;
